b3ff8f8dca9baced0f05147fd36c1e0672bc7f0c
jongsik <kjsik11@gmail.com>
2021-10-26 21:17:18 +0900

    make signin pgae

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -13,12 +13,6 @@ export default function SigninPage() {
               alt="Workflow"
             />
             <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-            <p className="mt-2 text-sm text-gray-600">
-              Or{' '}
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                start your 14-day free trial
-              </a>
-            </p>
           </div>
 
           <div className="mt-8">
